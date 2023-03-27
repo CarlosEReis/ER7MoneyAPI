318db5efa3f26b97aa622e0627e9377083a5ac77
CarlosEReis <carlos.er7@gmail.com>
2023-03-27 13:15:45 -0300

    03.01 - Criando o projeto da API

---
+++ src/test/java/com/carloser7/er7money/api/Er7moneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carloser7.er7money.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Er7moneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
