54a2c5d7476a75d97e97cfe085cfadccf98cdb5a
CarlosEReis <carlos.er7@gmail.com>
2023-03-27 17:53:35 -0300

    03.06 - Cadastrando nova categoria com POST

--- src/main/java/com/carloser7/er7money/api/resource/CategoriaResource.java
+++ src/main/java/com/carloser7/er7money/api/resource/CategoriaResource.java
@@ -1,11 +1,17 @@
 package com.carloser7.er7money.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.carloser7.er7money.api.model.Categoria;
 import com.carloser7.er7money.api.repository.CategoriaRepository;
@@ -16,6 +22,22 @@ public class CategoriaResource {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria) {
+		Categoria categoriaSalva = this.categoriaRepository.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+				.path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo())
+				.toUri();
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
+		return this.categoriaRepository.findById(codigo).orElse(null);
+	}
 	
 	// É uma boa prática retornar o status code 200 mesmo se o retorno do rescurso em questão estiver vazio.
 	@GetMapping
