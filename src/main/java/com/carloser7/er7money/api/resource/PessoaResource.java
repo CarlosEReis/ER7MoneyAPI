6d99b06ca3c37d5e378529576821cf46ddf0f45f
CarlosEReis <carlos.er7@gmail.com>
2023-03-28 11:59:57 -0300

    04.01 - Removendo pessoa com DELETE

--- src/main/java/com/carloser7/er7money/api/resource/PessoaResource.java
+++ src/main/java/com/carloser7/er7money/api/resource/PessoaResource.java
@@ -11,11 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carloser7.er7money.api.event.RecursoCriadoEvent;
@@ -56,4 +58,9 @@ public class PessoaResource {
 		return this.pessoaRepository.findAll();
 	}
 	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo ) {
+		this.pessoaRepository.deleteById(codigo);
+	}
 }
