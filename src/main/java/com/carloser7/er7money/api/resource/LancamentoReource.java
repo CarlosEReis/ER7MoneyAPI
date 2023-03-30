8029930c8f3c4e496bc802a52d1e1b67ddd6b9ec
CarlosEReis <carlos.er7@gmail.com>
2023-03-29 21:10:29 -0300

    05.03 - Desafio: Cadastrando o primeiro lançamento

--- src/main/java/com/carloser7/er7money/api/resource/LancamentoReource.java
+++ src/main/java/com/carloser7/er7money/api/resource/LancamentoReource.java
@@ -3,13 +3,21 @@ package com.carloser7.er7money.api.resource;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.carloser7.er7money.api.event.RecursoCriadoEvent;
 import com.carloser7.er7money.api.model.Lancamento;
 import com.carloser7.er7money.api.repository.LancamentoRepository;
 
@@ -19,6 +27,17 @@ public class LancamentoReource {
 
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
+	@PostMapping
+	public ResponseEntity<Lancamento> criar(@RequestBody @Valid Lancamento lancamento, HttpServletResponse reponse) {
+		
+		Lancamento lancamentoSalvo = this.lancamentoRepository.save(lancamento);
+		this.publisher.publishEvent(new RecursoCriadoEvent(this, reponse, lancamentoSalvo.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+	}
 	
 	@GetMapping("/{codigo}")
 	public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo) {
