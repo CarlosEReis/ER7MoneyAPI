0cc04be34c2298c1351d2d0bd221130a52bf3bb2
CarlosEReis <carlos.er7@gmail.com>
2023-03-30 17:18:38 -0300

    05.08 - Desafio: Removendo lançamentos

--- src/main/java/com/carloser7/er7money/api/resource/LancamentoResource.java
+++ src/main/java/com/carloser7/er7money/api/resource/LancamentoResource.java
@@ -13,12 +13,14 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carloser7.er7money.api.event.RecursoCriadoEvent;
@@ -31,7 +33,7 @@ import com.carloser7.er7money.api.service.exception.PessoaInexistenteOuInativaEx
 
 @RestController
 @RequestMapping("/lancamentos")
-public class LancamentoReource {
+public class LancamentoResource {
 
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
@@ -66,6 +68,12 @@ public class LancamentoReource {
 		return this.lancamentoRepository.filtrar(lancamentoFilter);
 	}
 	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		this.lancamentoRepository.deleteById(codigo);
+	}
+	
 	@ExceptionHandler({PessoaInexistenteOuInativaException.class})
 	public ResponseEntity<Object> handlerPessoaInexistenteOuInativaException(PessoaInexistenteOuInativaException ex) {
 		
@@ -75,4 +83,5 @@ public class LancamentoReource {
 		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
 		return ResponseEntity.badRequest().body(erros);
 	}
+	
 }
