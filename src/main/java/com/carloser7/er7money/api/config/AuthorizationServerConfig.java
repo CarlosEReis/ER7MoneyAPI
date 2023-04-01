863c8290254fcb71ce67740c04ca12bf3fa09a4c
CarlosEReis <carlos.er7@gmail.com>
2023-04-01 15:35:56 -0300

    06.05 - Configurando JWT no projeto

--- src/main/java/com/carloser7/er7money/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/carloser7/er7money/api/config/AuthorizationServerConfig.java
@@ -9,9 +9,9 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @SuppressWarnings("deprecation")
 @Configuration
@@ -43,19 +43,20 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints
-			.authenticationManager(this.authenticationManager)
-			.tokenStore(tokenStore());
+			.tokenStore(tokenStore())
+			.accessTokenConverter(jwtAccessTokenConverter())
+			.authenticationManager(this.authenticationManager);
 	}
 
-	@Override
-	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-		security.checkTokenAccess("permitAll()");
+	@Bean
+	public JwtAccessTokenConverter jwtAccessTokenConverter() {
+		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+		jwtAccessTokenConverter.setSigningKey("3032885ba9cd6621bcc4e7d6b6c35c2b");
+		return jwtAccessTokenConverter;
 	}
 	
 	@Bean
 	public TokenStore tokenStore() {
-		return new InMemoryTokenStore();
+		return new JwtTokenStore(jwtAccessTokenConverter());
 	}
-	
-	// Conhecendo um pouco sobre JWT (Json Web Token)
 }
