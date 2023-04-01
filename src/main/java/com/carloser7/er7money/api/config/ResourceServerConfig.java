038aaf7e931a807cd2df024d5633081bee030477
CarlosEReis <carlos.er7@gmail.com>
2023-04-01 17:57:23 -0300

    06.06 - Renovando o access token com o refresh token

--- src/main/java/com/carloser7/er7money/api/config/ResourceServerConfig.java
+++ src/main/java/com/carloser7/er7money/api/config/ResourceServerConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -79,6 +80,12 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 		return jwtAuthenticationConverter;
 	}
 
+	@Bean
+	@Override
+	public UserDetailsService userDetailsServiceBean() throws Exception {
+		return super.userDetailsServiceBean();
+	}
+	
 	@Bean
 	public JwtDecoder jwtDecoder() {
 		String secretKeyString = "3032885ba9cd6621bcc4e7d6b6c35c2b";
