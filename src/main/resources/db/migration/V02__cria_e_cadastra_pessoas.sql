eafdea91f69ef79785e69ba7971634069853b608
CarlosEReis <carlos.er7@gmail.com>
2023-04-04 12:59:47 -0300

    07.08 - Ajustando o CEP

--- src/main/resources/db/migration/V02__cria_e_cadastra_pessoas.sql
+++ src/main/resources/db/migration/V02__cria_e_cadastra_pessoas.sql
@@ -12,13 +12,13 @@ CREATE TABLE pessoa (
 ) ENGINE= InnoDB;
 
 INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values 
-('João Silva', 'Rua do Abacaxi', '10', null, 'Brasil', '38.400-12', 'Uberlândia', 'MG', true),
-('Maria Rita', 'Rua do Sabiá', '110', 'Apto 101', 'Colina', '11.400-12', 'Ribeirão Preto', 'SP', true),
-('Pedro Santos', 'Rua da Bateria', '23', null, 'Morumbi', '54.212-12', 'Goiânia', 'GO', true),
-('Ricardo Pereira', 'Rua do Motorista', '123', 'Apto 302', 'Aparecida', '38.400-12', 'Salvador', 'BA', true),
-('Josué Mariano', 'Av Rio Branco', '321', null, 'Jardins', '56.400-12', 'Natal', 'RN', true),
-('Pedro Barbosa', 'Av Brasil', '100', null, 'Tubalina', '77.400-12', 'Porto Alegre', 'RS', true),
-('Henrique Medeiros', 'Rua do Sapo', '1120', 'Apto 201', 'Centro', '12.400-12', 'Rio de Janeiro', 'RJ', true),
-('Carlos Santana', 'Rua da Manga', '433', null, 'Centro', '31.400-12', 'Belo Horizonte', 'MG', true),
-('Leonardo Oliveira', 'Rua do Músico', '566', null, 'Segismundo Pereira', '38.400-00', 'Uberlândia', 'MG', true),
-('Isabela Martins', 'Rua da Terra', '1233', 'Apto 10', 'Vigilato', '99.400-12', 'Manaus', 'AM', true);
+('João Silva', 'Rua do Abacaxi', '10', null, 'Brasil', '38400-121', 'Uberlândia', 'MG', true),
+('Maria Rita', 'Rua do Sabiá', '110', 'Apto 101', 'Colina', '11400-121', 'Ribeirão Preto', 'SP', true),
+('Pedro Santos', 'Rua da Bateria', '23', null, 'Morumbi', '54212-121', 'Goiânia', 'GO', true),
+('Ricardo Pereira', 'Rua do Motorista', '123', 'Apto 302', 'Aparecida', '38400-121', 'Salvador', 'BA', true),
+('Josué Mariano', 'Av Rio Branco', '321', null, 'Jardins', '56400-121', 'Natal', 'RN', true),
+('Pedro Barbosa', 'Av Brasil', '100', null, 'Tubalina', '77400-121', 'Porto Alegre', 'RS', true),
+('Henrique Medeiros', 'Rua do Sapo', '1120', 'Apto 201', 'Centro', '12400-121', 'Rio de Janeiro', 'RJ', true),
+('Carlos Santana', 'Rua da Manga', '433', null, 'Centro', '31400-121', 'Belo Horizonte', 'MG', true),
+('Leonardo Oliveira', 'Rua do Músico', '566', null, 'Segismundo Pereira', '38400-001', 'Uberlândia', 'MG', true),
+('Isabela Martins', 'Rua da Terra', '1233', 'Apto 10', 'Vigilato', '99400-121', 'Manaus', 'AM', true);
