8023c96ee6879c3cd18f47c51ce7e03be97d75d2
CarlosEReis <carlos.er7@gmail.com>
2023-05-23 18:37:11 -0300

    22.01 - Preparação do retorno dos dados para os gráficos

---
+++ src/main/java/com/carloser7/er7money/api/dto/LancamentoEstatisticaCategoria.java
@@ -0,0 +1,33 @@
+package com.carloser7.er7money.api.dto;
+
+import java.math.BigDecimal;
+
+import com.carloser7.er7money.api.model.Categoria;
+
+public class LancamentoEstatisticaCategoria {
+
+	private Categoria categoria;
+	private BigDecimal total;
+
+	public LancamentoEstatisticaCategoria(Categoria categoria, BigDecimal total) {
+		this.categoria = categoria;
+		this.total = total;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public BigDecimal getTotal() {
+		return total;
+	}
+
+	public void setTotal(BigDecimal total) {
+		this.total = total;
+	}
+
+}
