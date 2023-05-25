bf053d158c5b1ffbbdcf91ec5eefcca46f300333
CarlosEReis <carlos.er7@gmail.com>
2023-05-24 22:48:39 -0300

    22.13 - Gerando os bytes do relatório

--- src/main/java/com/carloser7/er7money/api/service/LancamentoService.java
+++ src/main/java/com/carloser7/er7money/api/service/LancamentoService.java
@@ -1,14 +1,25 @@
 package com.carloser7.er7money.api.service;
 
+import java.io.InputStream;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.carloser7.er7money.api.dto.LancamentoEstatisticaPessoa;
 import com.carloser7.er7money.api.model.Lancamento;
 import com.carloser7.er7money.api.repository.LancamentoRepository;
 import com.carloser7.er7money.api.service.exception.RecursoInexistenteException;
+
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @Service
 public class LancamentoService {
@@ -37,6 +48,19 @@ public class LancamentoService {
 		return this.lancamentoRepository.save(lancamentoBanco);
 	}
 
+	public byte[] relatoriaPorPessoa(LocalDate inicio, LocalDate fim) throws Exception {
+		List<LancamentoEstatisticaPessoa> dados = this.lancamentoRepository.porPessoa(inicio, fim);
+		
+		HashMap<String, Object> parametros = new HashMap<>();
+		parametros.put("DT_INICIAL", Date.valueOf(inicio));
+		parametros.put("DT_FINAL", Date.valueOf(fim));
+		
+		InputStream inputStream = this.getClass().getResourceAsStream("/relatorios/lancamentos-por-pessoa-01.jasper");
+		JasperPrint jasperPrint = JasperFillManager.fillReport(inputStream, parametros, new  JRBeanCollectionDataSource(dados));
+		
+		return JasperExportManager.exportReportToPdf(jasperPrint);
+	}
+	
 	private Lancamento buscaLancamento(Long codigo) {
 		Optional<Lancamento> lancamento = this.lancamentoRepository.findById(codigo);
 		if (lancamento.isEmpty()) {
@@ -44,11 +68,5 @@ public class LancamentoService {
 		}
 		return lancamento.get();
 	}
-
-
- 
-
-	
-
 	
 }
