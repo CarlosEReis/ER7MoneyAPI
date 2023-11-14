05ee382b23cc95ecb80a4e33c18321d0fee921b8
CarlosEReis <carlos.er7@gmail.com>
2023-11-14 11:17:35 -0300

    build: add dados mes atual para plotar graficos

--- src/main/resources/db/migration/V03__cria_e_registra_lancamentos.sql
+++ src/main/resources/db/migration/V03__cria_e_registra_lancamentos.sql
@@ -13,7 +13,7 @@ CREATE TABLE lancamento (
 ) ENGINE=InnoDb;
 
 INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values 
-('Salário mensal', '2022-05-09', null, 6500.00, 'Distribuição de lucros', 'RECEITA', 1, 1),
+('Salário mensal', '2022-05-09', null, 2500.00, 'Distribuição de lucros', 'RECEITA', 1, 1),
 ('Bahamas', '2022-02-10', '2022-02-10', 100.32, null, 'DESPESA', 2, 2),
 ('Top Club', '2022-03-10', null, 120, null, 'RECEITA', 3, 3),
 ('CEMIG', '2022-05-02', '2022-02-10', 110.44, 'Geração', 'RECEITA', 3, 4),
@@ -27,4 +27,35 @@ INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, obser
 ('Eletrônicos', '2022-01-10', '2022-04-10', 2100.32, null, 'DESPESA', 5, 4),
 ('Instrumentos', '2022-05-04', null, 1040.32, null, 'DESPESA', 4, 3),
 ('Café', '2022-03-10', '2022-04-10', 4.32, null, 'DESPESA', 4, 2),
-('Lanche', '2022-02-10', null, 10.20, null, 'DESPESA', 4, 1);
+('Lanche', '2022-02-10', null, 10.20, null, 'DESPESA', 4, 1),
+
+('Salário mensal', DATE_FORMAT(NOW(), '%Y-%m-01'), null, 2500.00, 'Distribuição de lucros', 'RECEITA', 1, 1),
+('Bahamas', DATE_FORMAT(NOW(), '%Y-%m-02'), DATE_FORMAT(NOW(), '%Y-%m-10'), 100.32, null, 'DESPESA', 2, 2),
+('Top Club', DATE_FORMAT(NOW(), '%Y-%m-03'), null, 120, null, 'RECEITA', 3, 3),
+('CEMIG', DATE_FORMAT(NOW(), '%Y-%m-04'), DATE_FORMAT(NOW(), '%Y-%m-10'), 110.44, 'Geração', 'RECEITA', 3, 4),
+('DMAE', DATE_FORMAT(NOW(), '%Y-%m-05'), null, 200.30, null, 'DESPESA', 3, 5),
+('Extra', DATE_FORMAT(NOW(), '%Y-%m-06'), DATE_FORMAT(NOW(), '%Y-%m-10'), 1010.32, null, 'RECEITA', 4, 6),
+('Bahamas', DATE_FORMAT(NOW(), '%Y-%m-07'), null, 500, null, 'RECEITA', 1, 7),
+('Top Club', DATE_FORMAT(NOW(), '%Y-%m-08'), DATE_FORMAT(NOW(), '%Y-%m-10'), 400.32, null, 'DESPESA', 4, 8),
+('Despachante', DATE_FORMAT(NOW(), '%Y-%m-09'), null, 123.64, 'Multas', 'DESPESA', 3, 9),
+('Pneus', DATE_FORMAT(NOW(), '%Y-%m-10'), DATE_FORMAT(NOW(), '%Y-%m-10'), 665.33, null, 'RECEITA', 5, 10),
+('Café', DATE_FORMAT(NOW(), '%Y-%m-11'), null, 8.32, null, 'DESPESA', 1, 5),
+('Eletrônicos', DATE_FORMAT(NOW(), '%Y-%m-12'), DATE_FORMAT(NOW(), '%Y-%m-10'), 2100.32, null, 'DESPESA', 5, 4),
+('Instrumentos', DATE_FORMAT(NOW(), '%Y-%m-13'), null, 1040.32, null, 'DESPESA', 4, 3),
+('Café', DATE_FORMAT(NOW(), '%Y-%m-14'), DATE_FORMAT(NOW(), '%Y-%m-10'), 4.32, null, 'DESPESA', 4, 2),
+('Lanche', DATE_FORMAT(NOW(), '%Y-%m-15'), null, 10.20, null, 'DESPESA', 4, 1),
+
+('Bahamas', DATE_FORMAT(NOW(), '%Y-%m-16'), DATE_FORMAT(NOW(), '%Y-%m-10'), 100.32, null, 'DESPESA', 2, 2),
+('Top Club', DATE_FORMAT(NOW(), '%Y-%m-17'), null, 120, null, 'RECEITA', 3, 3),
+('CEMIG', DATE_FORMAT(NOW(), '%Y-%m-18'), DATE_FORMAT(NOW(), '%Y-%m-10'), 110.44, 'Geração', 'RECEITA', 3, 4),
+('DMAE', DATE_FORMAT(NOW(), '%Y-%m-19'), null, 200.30, null, 'DESPESA', 3, 5),
+('Extra', DATE_FORMAT(NOW(), '%Y-%m-20'), DATE_FORMAT(NOW(), '%Y-%m-10'), 1010.32, null, 'RECEITA', 4, 6),
+('Bahamas', DATE_FORMAT(NOW(), '%Y-%m-21'), null, 500, null, 'RECEITA', 1, 7),
+('Top Club', DATE_FORMAT(NOW(), '%Y-%m-22'), DATE_FORMAT(NOW(), '%Y-%m-10'), 400.32, null, 'DESPESA', 4, 8),
+('Despachante', DATE_FORMAT(NOW(), '%Y-%m-23'), null, 123.64, 'Multas', 'DESPESA', 3, 9),
+('Pneus', DATE_FORMAT(NOW(), '%Y-%m-24'), DATE_FORMAT(NOW(), '%Y-%m-10'), 665.33, null, 'RECEITA', 5, 10),
+('Café', DATE_FORMAT(NOW(), '%Y-%m-25'), null, 8.32, null, 'DESPESA', 1, 5),
+('Eletrônicos', DATE_FORMAT(NOW(), '%Y-%m-26'), DATE_FORMAT(NOW(), '%Y-%m-10'), 2100.32, null, 'DESPESA', 5, 4),
+('Instrumentos', DATE_FORMAT(NOW(), '%Y-%m-27'), null, 1040.32, null, 'DESPESA', 4, 3),
+('Café', DATE_FORMAT(NOW(), '%Y-%m-28'), DATE_FORMAT(NOW(), '%Y-%m-10'), 4.32, null, 'DESPESA', 4, 2),
+('Lanche', DATE_FORMAT(NOW(), '%Y-%m-29'), null, 10.20, null, 'DESPESA', 4, 1);
